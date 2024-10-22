898b8eaa3381c61d1818361e870dadc2324e80db
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-10-22 17:57:26 +0600

    feat(Error): add JSON parse Error and handle default error as Server Error

---
+++ src/error/Error.ts
@@ -0,0 +1,7 @@
+export interface JsonParseError {
+  expose: boolean;
+  statusCode: number;
+  status: number;
+  body: string;
+  type: string;
+}
--- src/error/index.ts
+++ src/error/index.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpError } from 'http-errors';
 import { ZodError } from 'zod';
+import { JsonParseError } from '@error/Error';
 
 export function handleError(
-  err: HttpError | ZodError,
+  err: HttpError | ZodError | JsonParseError,
   req: Request,
   res: Response,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -28,5 +29,18 @@ export function handleError(
     return;
   }
 
-  res.status(400).json(err);
+  if (err.type === 'entity.parse.failed') {
+    res.status(400).json({
+      stauts: 'fail',
+      message: 'Input is in Invalid JSON format',
+    });
+    return;
+  }
+
+  // log error in server and send a common error response as fallback
+  console.log(err);
+  res.status(500).json({
+    status: 'fail',
+    message: 'something went wrong!',
+  });
 }

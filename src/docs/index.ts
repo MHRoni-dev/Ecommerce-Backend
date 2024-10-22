0fd507865f8026b313ed71c69e885efc228a28df
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-10-22 17:57:59 +0600

    feat(docs): add product create documentation

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -1,8 +1,28 @@
 import swaggerUI from 'swagger-ui-express';
-import path from 'path';
 import YAML from 'yamljs';
+import path from 'path';
 
-const swaggerDocument = YAML.load(path.join('src', 'docs', 'swagger.yaml'));
+let swaggerDocument;
+let productDocs;
+
+try {
+  // Load the main OpenAPI YAML
+  swaggerDocument = YAML.load(path.join('src', 'docs', 'swagger.yaml'));
+
+  // Load the product-specific YAML
+  productDocs = YAML.load(
+    path.join('src', 'app', 'v1', 'product', 'docs.yaml'),
+  );
+
+  // Merge the product-specific docs into the main OpenAPI document
+  Object.assign(swaggerDocument, productDocs);
+} catch (error) {
+  console.error('Error loading or merging YAML files:', error);
+
+  // You can initialize an empty object or handle the error as needed
+  swaggerDocument = {}; // Initialize to an empty object or provide fallback
+}
+
 const documentation = [swaggerUI.serve, swaggerUI.setup(swaggerDocument)];
 
 export default documentation;

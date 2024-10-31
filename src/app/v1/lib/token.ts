b78da2dcb32c9546b1b0db96baff99294961aa2a
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-10-31 23:00:56 +0600

    feat(security): add hashing and token system

---
+++ src/app/v1/lib/token.ts
@@ -0,0 +1,38 @@
+import config from '@config/index';
+import jwt from 'jsonwebtoken';
+
+interface TokenPayload {
+  _id: string;
+  email: string;
+}
+
+interface DecodedToken extends TokenPayload, jwt.JwtPayload {}
+
+export function generateTokenAsync(payload: TokenPayload): Promise<string> {
+  return new Promise((resolve, reject) => {
+    jwt.sign(
+      payload,
+      config.JWT.SECRET,
+      { expiresIn: config.JWT.EXPIRES_IN },
+      (err, token) => {
+        if (err || !token) {
+          return reject(err);
+        } else {
+          return resolve(token);
+        }
+      },
+    );
+  });
+}
+
+export function verifyTokenAsync(token: string): Promise<DecodedToken> {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, config.JWT.SECRET, (err, decoded) => {
+      if (err || !decoded) {
+        return reject(err);
+      } else {
+        return resolve(decoded as DecodedToken);
+      }
+    });
+  });
+}

86c0299349e3b6d4d04fa1d8ac9e11df561ca819
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-11-02 14:51:58 +0600

    test(user): add user verification and verification check test

--- tests/e2e/user.e2e.test.ts
+++ tests/e2e/user.e2e.test.ts
@@ -3,6 +3,7 @@ import app from '@src/app';
 import { UserCreateInput, UserLoginInput } from '@src/app/v1/types';
 import request from 'supertest';
 import { invalidNewUser, validNewUser } from '@e2e/__fixtures__/userData';
+import { AuthModel } from '@src/app/v1/user/model';
 
 beforeAll(async () => {
   await connectDB();
@@ -22,6 +23,14 @@ async function registerUser(data: UserCreateInput) {
 
 async function loginUser(data: UserLoginInput) {
   return await request(app).post('/api/v1/user/login').send(data);
+}
+
+async function verifyUser(email: string, otp?: string, token?: string) {
+  return await request(app)
+    .get(
+      `/api/v1/user/verify/${email}?${otp ? `otp=${otp}` : ''}${token ? `&token=${token}` : ''}`,
+    )
+    .send();
 }
 
 describe('User E2E test', () => {
@@ -49,9 +58,97 @@ describe('User E2E test', () => {
     expect(secondRes.body).toHaveProperty('message');
   });
 
+  it('should not login unverified user', async () => {
+    const res = await registerUser(validNewUser);
+    expect(res.status).toBe(201);
+
+    const loginRes = await loginUser(validNewUser);
+    expect(loginRes.status).toBe(403);
+    expect(loginRes.body).toHaveProperty('message');
+  });
+
+  it('should verify otp', async () => {
+    const res = await registerUser(validNewUser);
+    expect(res.status).toBe(201);
+
+    const auth = await AuthModel.findOne({ email: validNewUser.email });
+
+    const verifyRes = await verifyUser(validNewUser.email, auth?.otp);
+    expect(verifyRes.status).toBe(200);
+  });
+
+  it('should verify token', async () => {
+    const res = await registerUser(validNewUser);
+    expect(res.status).toBe(201);
+
+    const auth = await AuthModel.findOne({ email: validNewUser.email });
+
+    const verifyRes = await verifyUser(
+      validNewUser.email,
+      undefined,
+      auth?.token,
+    );
+    expect(verifyRes.status).toBe(200);
+  });
+
+  it('should handle wrong otp', async () => {
+    const res = await registerUser(validNewUser);
+    expect(res.status).toBe(201);
+
+    const verifyRes = await verifyUser(validNewUser.email, 'wrong');
+    expect(verifyRes.status).toBe(400);
+  });
+
+  it('should handle wrong token', async () => {
+    const res = await registerUser(validNewUser);
+    expect(res.status).toBe(201);
+
+    const verifyRes = await verifyUser(validNewUser.email, undefined, 'wrong');
+    expect(verifyRes.status).toBe(400);
+  });
+
+  it('should handle used otp', async () => {
+    const res = await registerUser(validNewUser);
+    expect(res.status).toBe(201);
+
+    const auth = await AuthModel.findOne({ email: validNewUser.email });
+
+    const verifyRes = await verifyUser(validNewUser.email, auth?.otp);
+    expect(verifyRes.status).toBe(200);
+
+    const verifyRes2 = await verifyUser(validNewUser.email, auth?.otp);
+    expect(verifyRes2.status).toBe(403);
+  });
+
+  it('should handle used token', async () => {
+    const res = await registerUser(validNewUser);
+    expect(res.status).toBe(201);
+
+    const auth = await AuthModel.findOne({ email: validNewUser.email });
+
+    const verifyRes = await verifyUser(
+      validNewUser.email,
+      undefined,
+      auth?.token,
+    );
+    expect(verifyRes.status).toBe(200);
+
+    const verifyRes2 = await verifyUser(
+      validNewUser.email,
+      undefined,
+      auth?.token,
+    );
+    expect(verifyRes2.status).toBe(403);
+  });
+
   it('should login user', async () => {
     const res = await registerUser(validNewUser);
     expect(res.status).toBe(201);
+
+    const auth = await AuthModel.findOne({ email: validNewUser.email });
+
+    const verifyRes = await verifyUser(validNewUser.email, auth?.otp);
+    expect(verifyRes.status).toBe(200);
 
     const loginRes = await loginUser(validNewUser);
     expect(loginRes.status).toBe(200);
@@ -60,7 +157,7 @@ describe('User E2E test', () => {
     expect(loginRes.body).toHaveProperty('accessToken');
   });
 
-  it('should not login user', async () => {
+  it('should not login invalid user', async () => {
     const res = await loginUser(invalidNewUser);
     expect(res.status).toBe(400);
     expect(res.body).toHaveProperty('message');
@@ -70,6 +167,11 @@ describe('User E2E test', () => {
     const res = await registerUser(validNewUser);
     expect(res.status).toBe(201);
 
+    const auth = await AuthModel.findOne({ email: validNewUser.email });
+
+    const verifyRes = await verifyUser(validNewUser.email, auth?.otp);
+    expect(verifyRes.status).toBe(200);
+
     const loginRes = await loginUser({ ...validNewUser, password: 'wrong' });
     expect(loginRes.status).toBe(400);
     expect(loginRes.body).toHaveProperty('message');
@@ -78,6 +180,11 @@ describe('User E2E test', () => {
   it('should not response with password', async () => {
     const res = await registerUser(validNewUser);
     expect(res.status).toBe(201);
+
+    const auth = await AuthModel.findOne({ email: validNewUser.email });
+
+    const verifyRes = await verifyUser(validNewUser.email, auth?.otp);
+    expect(verifyRes.status).toBe(200);
 
     const loginRes = await loginUser(validNewUser);
     expect(loginRes.status).toBe(200);

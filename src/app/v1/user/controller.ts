f835a8eb8db5a9aee3561d50d99a5bc73ee4b20c
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-11-08 05:11:08 +0600

    refactor,feat(user): refactor resendverification and register with otp purpose and add password reset endpoint

--- src/app/v1/user/controller.ts
+++ src/app/v1/user/controller.ts
@@ -4,6 +4,7 @@ import { UserCreateInput, UserLoginInput } from '@v1/types';
 import {
   userCreateInputZodSchema,
   userLoginInputZodSchema,
+  validVerificationReqPurposeZodSchema,
 } from '@v1/user/schema';
 import { AuthModel, UserModel } from '@v1/user/model';
 import createHttpError from 'http-errors';
@@ -68,8 +69,10 @@ export async function registerUser(
         { email: userInputData.email },
         {
           email: userInputData.email,
+          purpose: 'emailVerification',
           otp,
           token,
+          isUsed: false,
           expiresAt: new Date(Date.now() + config.SECURITY.OTP_DURATION),
         },
         { session, upsert: true },
@@ -121,6 +124,16 @@ export async function resendVerification(
       throw createHttpError.BadRequest('Invalid email');
     }
 
+    // >> check purpose
+    const purpose = req.body.purpose;
+    const isValidPurpose =
+      await validVerificationReqPurposeZodSchema.safeParseAsync(purpose);
+    if (!isValidPurpose.success) {
+      throw createHttpError.BadRequest(
+        'Invalid purpose of verification request',
+      );
+    }
+
     // >> check if user exist
     const userExist = await UserModel.findOne(
       {
@@ -145,9 +158,14 @@ export async function resendVerification(
       { email: isEmail.data },
       {
         email: isEmail.data,
+        purpose: isValidPurpose.data,
+        isUsed: false,
         otp,
         token,
         expiresAt: new Date(Date.now() + config.SECURITY.OTP_DURATION),
+      },
+      {
+        upsert: true,
       },
     );
 
@@ -164,7 +182,7 @@ export async function resendVerification(
     res.status(200).json({
       status: 'success',
       message: 'Verification sent successfully',
-      data: 'Check your email to verify your account',
+      data: 'Check your email for verification',
     });
 
     // end of function
@@ -318,6 +336,86 @@ export async function loginUser(
       message: 'User logged in successfully',
       user: userExist,
       accessToken: token,
+    });
+
+    //end of function
+    return;
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function resetPassword(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    // >> check if valid email , password and otp or token is provided
+    const email = req.body.email;
+    const isEmail = await z.string().email().safeParseAsync(email);
+    if (!isEmail.success) {
+      throw createHttpError.BadRequest('Invalid email');
+    }
+    const password = req.body.password;
+    if (!password) {
+      throw createHttpError.BadRequest('Password is required');
+    }
+    const otp = req.body.otp;
+    const token = req.body.token;
+    if (!otp && !token) {
+      throw createHttpError.BadRequest('otp or token is required');
+    }
+
+    // >> check if user exist
+    const userExist = await UserModel.findOne({ email: isEmail.data });
+    if (!userExist) {
+      throw createHttpError.BadRequest('Register your account first');
+    }
+
+    // >> check if otp or token is not expired and have the same purpose (expired data doesn't exist)
+    const authExist = await AuthModel.findOne({
+      email: isEmail.data,
+      purpose: 'passwordReset',
+      $or: [{ otp }, { token }],
+    });
+    if (!authExist) {
+      throw createHttpError.BadRequest('Invalid otp or token');
+    }
+
+    // >> check if otp or token is already used
+    if (authExist.isUsed) {
+      throw createHttpError.Forbidden('otp or token already used');
+    }
+
+    // >> update password and isUsed
+    let session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+      const hashedPassword = await hashPassword(password);
+      await UserModel.findOneAndUpdate(
+        { email: isEmail.data },
+        { password: hashedPassword },
+        { session },
+      );
+      await AuthModel.findOneAndUpdate(
+        { email: isEmail.data },
+        { isUsed: true },
+        { session },
+      );
+      await session.commitTransaction();
+    } catch (error) {
+      console.log('Password reset failed: ', error);
+      await session.abortTransaction();
+      throw createHttpError.InternalServerError('something went wrong');
+    } finally {
+      await session.endSession();
+    }
+
+    // >> response
+    res.status(200).json({
+      status: 'success',
+      message: 'Password reset successfully',
     });
 
     //end of function
--- src/app/v1/user/routes.ts
+++ src/app/v1/user/routes.ts
@@ -7,5 +7,6 @@ userRoutes.post('/register', controller.registerUser);
 userRoutes.post('/resend-verification/', controller.resendVerification);
 userRoutes.get('/verify/:email', controller.verifyUser);
 userRoutes.post('/login', controller.loginUser);
+userRoutes.put('/reset-password', controller.resetPassword);
 
 export default userRoutes;
